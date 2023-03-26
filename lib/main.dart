7b0b8ca2b1f3a4f69e12d61237cae68887557a8a
Faridz <faridzkurniaa@gmail.com>
2023-03-26 13:54:44 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bank_purse2/ui/pages/onboarding_page.dart';
+import 'package:bank_purse2/ui/pages/sign_in_page.dart';
+import 'package:bank_purse2/ui/pages/splash_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashPage(),
+        '/onboarding': (context) => const OnboardingPage(),
+        '/sign-in': (context) => SignInPage(),
+      },
+    );
+  }
+}
---
+++ lib/ui/pages/onboarding_page.dart
@@ -0,0 +1,192 @@
+import 'package:bank_purse2/shared/theme.dart';
+import 'package:bank_purse2/ui/pages/sign_in_page.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/buttons.dart';
+
+class OnboardingPage extends StatefulWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  _OnboardingPageState createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  int CurrentIndex = 0;
+  CarouselController carouselController = CarouselController();
+
+  List<String> titles = [
+    'Grow Your\nFinancial Today',
+    'Build from\nzero to freedom',
+    'Start Together',
+  ];
+
+  List<String> subTitles = [
+    'Our system is helping you to\nachieve a better goal',
+    'We provide tips for you so that\nyou can adapt easier',
+    'We will guided you to where\nyou wanted it too',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CarouselSlider(
+              items: [
+                Image.asset(
+                  'assets/img_onboarding1.png',
+                  height: 331,
+                ),
+                Image.asset(
+                  'assets/img_onboarding2.png',
+                  height: 331,
+                ),
+                Image.asset(
+                  'assets/img_onboarding3.png',
+                  height: 331,
+                ),
+              ],
+              options: CarouselOptions(
+                height: 331,
+                viewportFraction: 1,
+                enableInfiniteScroll: false,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    CurrentIndex = index;
+                  });
+                },
+              ),
+              carouselController: carouselController,
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 24,
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 22,
+                vertical: 24,
+              ),
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    titles[CurrentIndex],
+                    style: blackTextStyle.copyWith(
+                      fontSize: 20,
+                      fontWeight: semiBold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 26,
+                  ),
+                  Text(
+                    subTitles[CurrentIndex],
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: CurrentIndex == 2 ? 38 : 50,
+                  ),
+                  CurrentIndex == 2
+                      ? Column(
+                          children: [
+                            CustomFilledButton(
+                              title: 'Get Started',
+                              onPressed: () {},
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            SizedBox(
+                              width: double.infinity,
+                              height: 24,
+                              child: TextButton(
+                                onPressed: () {
+                                  Navigator.pushNamed(context, '/sign-in');
+                                },
+                                style: TextButton.styleFrom(
+                                    padding: EdgeInsets.zero),
+                                child: Text(
+                                  'Sign In',
+                                  style: greyTextStyle.copyWith(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      : Row(
+                          children: [
+                            Container(
+                              width: 12,
+                              height: 12,
+                              margin: const EdgeInsets.only(
+                                right: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: CurrentIndex == 0
+                                    ? blueColor
+                                    : lightBackgroundColor,
+                              ),
+                            ),
+                            Container(
+                              width: 12,
+                              height: 12,
+                              margin: const EdgeInsets.only(
+                                right: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: CurrentIndex == 1
+                                    ? blueColor
+                                    : lightBackgroundColor,
+                              ),
+                            ),
+                            Container(
+                              width: 12,
+                              height: 12,
+                              margin: const EdgeInsets.only(
+                                right: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: CurrentIndex == 2
+                                    ? blueColor
+                                    : lightBackgroundColor,
+                              ),
+                            ),
+                            const Spacer(),
+                            CustomFilledButton(
+                              width: 150,
+                              title: 'Continue',
+                              onPressed: () {
+                                carouselController.nextPage();
+                              },
+                            ),
+                          ],
+                        ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
