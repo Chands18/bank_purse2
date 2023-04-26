5e613620e1799c813f215fa666619e463d02781c
Faridz <faridzkurniaa@gmail.com>
2023-04-26 18:49:13 +0700

    edit profile page

--- lib/ui/pages/profile_edit_page.dart
+++ lib/ui/pages/profile_edit_page.dart
@@ -1,3 +1,6 @@
+import 'package:bank_purse2/shared/theme.dart';
+import 'package:bank_purse2/ui/widgets/buttons.dart';
+import 'package:bank_purse2/ui/widgets/forms.dart';
 import 'package:flutter/material.dart';
 
 class ProfileEditPage extends StatelessWidget {
@@ -5,6 +8,77 @@ class ProfileEditPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Edit Profile',
+        ),
+      ),
+      body: ListView(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: whiteColor,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                CustomFormField(
+                  title: 'Username',
+                  // controller: usernameController,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                CustomFormField(
+                  title: 'Full Name',
+                  // controller: nameController,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                CustomFormField(
+                  title: 'Email Address',
+                  // controller: emailController,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                CustomFormField(
+                  title: 'Password',
+                  obscureText: true,
+                  // controller: passwordController,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                CustomFilledButton(
+                  title: 'Update Now',
+                  // onPressed: () {
+                  //   // context.read<AuthBloc>().add(
+                  //   //       AuthUpdateUser(
+                  //   //         widget.user,
+                  //   //         UserEditFormModel(
+                  //   //           email: emailController.text,
+                  //   //           name: nameController.text,
+                  //   //           username: usernameController.text,
+                  //   //           password: passwordController.text,
+                  //   //           pin: widget.user.pin,
+                  //   //         ),
+                  //   //       ),
+                  //   //     );
+                  // },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
