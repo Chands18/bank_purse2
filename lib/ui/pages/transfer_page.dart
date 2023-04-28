7336659ae5c7878b9b37d0e671837d236597980c
Faridz <faridzkurniaa@gmail.com>
2023-04-28 15:50:59 +0700

    recent user

--- lib/ui/pages/transfer_page.dart
+++ lib/ui/pages/transfer_page.dart
@@ -1,5 +1,6 @@
 import 'package:bank_purse2/shared/theme.dart';
 import 'package:bank_purse2/ui/widgets/forms.dart';
+import 'package:bank_purse2/ui/widgets/transfer_recent_user_item.dart';
 import 'package:flutter/material.dart';
 
 class TransferPage extends StatelessWidget {
@@ -31,7 +32,7 @@ class TransferPage extends StatelessWidget {
           const SizedBox(
             height: 14,
           ),
-          CustomFormField(
+          const CustomFormField(
             title: 'by username',
             isShowTitle: false,
             // controller: usernameController,
@@ -44,7 +45,7 @@ class TransferPage extends StatelessWidget {
             //   setState(() {});
             // },
           ),
-          // buildRecentUsers(),
+          buildRecentUsers(),
           // usernameController.text.isEmpty ? buildRecentUsers() : buildResult(),
           const SizedBox(
             height: 80,
@@ -72,4 +73,118 @@ class TransferPage extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget buildRecentUsers() {
+  return Container(
+    margin: const EdgeInsets.only(
+      top: 40,
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Recent Users',
+          style: blackTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: semiBold,
+          ),
+        ),
+        const SizedBox(
+          height: 14,
+        ),
+        const TransferRecentUserItem(
+          imageUrl: 'assets/img_friend1.png',
+          isVerified: true,
+          name: 'Victoria',
+          username: 'Vpfifer',
+        ),
+        const TransferRecentUserItem(
+          imageUrl: 'assets/img_friend2.png',
+          isVerified: false,
+          name: 'Victoria',
+          username: 'Vpfifer',
+        )
+        // BlocBuilder<UserBloc, UserState>(
+        //   builder: (context, state) {
+        //     if (state is UserSuccess) {
+        //       return Column(
+        //         children: state.users.map((user) {
+        //           return GestureDetector(
+        //             onTap: () {
+        //               Navigator.push(
+        //                 context,
+        //                 MaterialPageRoute(
+        //                   builder: (context) => TransferAmountPage(
+        //                     data: TransferFormModel(
+        //                       sendTo: user.username,
+        //                     ),
+        //                   ),
+        //                 ),
+        //               );
+        //             },
+        //             child: TransferRecentUserItem(user: user),
+        //           );
+        //         }).toList(),
+        //       );
+        //     }
+
+        //     return const Center(
+        //       child: CircularProgressIndicator(),
+        //     );
+        //   },
+        // ),
+      ],
+    ),
+  );
+}
+
+Widget buildResult() {
+  return Container(
+    margin: const EdgeInsets.only(
+      top: 40,
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Result',
+          style: blackTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: semiBold,
+          ),
+        ),
+        const SizedBox(
+          height: 14,
+        ),
+        // BlocBuilder<UserBloc, UserState>(
+        //   builder: (context, state) {
+        //     if (state is UserSuccess) {
+        //       return Wrap(
+        //         spacing: 17,
+        //         runSpacing: 17,
+        //         children: state.users.map((user) {
+        //           return GestureDetector(
+        //             onTap: () {
+        //               setState(() {
+        //                 selectedUser = user;
+        //               });
+        //             },
+        //             child: TransferResultUserItem(
+        //               user: user,
+        //               isSelected: user.id == selectedUser?.id,
+        //             ),
+        //           );
+        //         }).toList(),
+        //       );
+        //     }
+
+        //     return const Center(
+        //       child: CircularProgressIndicator(),
+        //     );
+        //   },
+        // ),
+      ],
+    ),
+  );
 }
