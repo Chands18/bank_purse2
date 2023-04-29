771b9e515636fc14a3ce8716e0c6be81f3ad8e08
Faridz <faridzkurniaa@gmail.com>
2023-04-29 21:32:20 +0700

    data page modal 1

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -316,10 +316,10 @@ class HomePage extends StatelessWidget {
                 iconUrl: 'assets/ic_more.png',
                 title: 'More',
                 onTap: () {
-                  // showDialog(
-                  //   context: context,
-                  //   builder: (context) => const MoreDialog(),
-                  // );
+                  showDialog(
+                    context: context,
+                    builder: (context) => const MoreDialog(),
+                  );
                 },
               ),
             ],
@@ -535,6 +535,83 @@ class HomePage extends StatelessWidget {
           //   ),
           // ),
         ],
+      ),
+    );
+  }
+}
+
+class MoreDialog extends StatelessWidget {
+  const MoreDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.transparent,
+      insetPadding: EdgeInsets.zero,
+      alignment: Alignment.bottomCenter,
+      content: Container(
+        height: 329,
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(30),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40),
+          color: lightBackgroundColor,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Do More With Us',
+              style: blackTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semiBold,
+              ),
+            ),
+            const SizedBox(
+              height: 9,
+            ),
+            Center(
+              child: Wrap(
+                spacing: 29,
+                runSpacing: 25,
+                children: [
+                  HomeServiceItem(
+                    iconUrl: 'assets/ic_product_data.png',
+                    title: 'Data',
+                    onTap: () {
+                      Navigator.pushNamed(context, '/data-provider');
+                    },
+                  ),
+                  HomeServiceItem(
+                    iconUrl: 'assets/ic_product_water.png',
+                    title: 'Water',
+                    onTap: () {},
+                  ),
+                  HomeServiceItem(
+                    iconUrl: 'assets/ic_product_stream.png',
+                    title: 'Stream',
+                    onTap: () {},
+                  ),
+                  HomeServiceItem(
+                    iconUrl: 'assets/ic_product_movie.png',
+                    title: 'Movie',
+                    onTap: () {},
+                  ),
+                  HomeServiceItem(
+                    iconUrl: 'assets/ic_product_food.png',
+                    title: 'Food',
+                    onTap: () {},
+                  ),
+                  HomeServiceItem(
+                    iconUrl: 'assets/ic_product_travel.png',
+                    title: 'Travel',
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
